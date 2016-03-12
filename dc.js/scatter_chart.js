64a9bb821eeeb54b1b175136678c0681beac54d8
Axel von Engel <a.vonengel@gmail.com>
2016-03-12 22:02:38 +0100

    Add dynamic data change

--- dc.js/scatter_chart.js
+++ dc.js/scatter_chart.js
@@ -1,7 +1,7 @@
 var chart = dc.scatterPlot("#scatter-chart");
 
-var data = d3.range(1000).map(function() {
- return d3.range(2).map(Math.random);
+var data = d3.range(10).map(function() {
+  return d3.range(2).map(Math.random);
 });
 
 
@@ -16,17 +16,26 @@ chart
   .height(480)
   .x(d3.scale.linear())
   .y(d3.scale.linear())
-  // .keyAccessor(function(p) {
-  //   return p[0];
-  // })
-  // .valueAccessor(function(p) {
-  //   return p[1];
-  // })
-  //.brushOn(false)
   .symbolSize(8)
   .clipPadding(10)
   .yAxisLabel("This is the Y Axis!")
   .dimension(xDimension)
+  .transitionDuration(0)
   .group(allGroup);
 
 chart.render();
+
+function addData() {
+  var countToAdd = +document.getElementById('numberOfItems').value;
+  var newData = d3.range(countToAdd).map(function() {
+    return d3.range(2).map(Math.random);
+  });
+  ndx.add(newData);
+  chart.redraw();
+}
+document.getElementById("addButton").onclick = addData;
+
+document.getElementById("clearButton").onclick = function() {
+  ndx.remove();
+  chart.redraw();
+}
